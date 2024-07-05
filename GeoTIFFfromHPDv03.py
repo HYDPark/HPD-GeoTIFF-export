508ef5d58b4146482c6331f2c98ab8f15bdcf867
Daehyun Park <DPark@linz.govt.nz>
2024-07-05 14:31:32 +1200

    Description updsate

--- GeoTIFFfromHPDv03.py
+++ GeoTIFFfromHPDv03.py
@@ -1,1 +1,12 @@
-## New Edition2##
+## Edition2 for Manual Publishing
+## Remove LDS Publishing Module
+## Change Error control -- User notification for data duplication and fixing
+###########################################################################
+# Required
+# Caris HPD PaperChartBuilder License 
+# Oracle Client for HPD connection
+# N(Mariners) Drive connection for Carisbatch running.
+###########################################################################
+
+
+
