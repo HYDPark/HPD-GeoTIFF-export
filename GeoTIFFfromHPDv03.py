a8b5a327e8d4c00ddba313b61bdb72e6a3f64897
Daehyun Park <DPark@linz.govt.nz>
2024-07-15 09:37:16 +1200

    sys migration test

--- GeoTIFFfromHPDv03.py
+++ GeoTIFFfromHPDv03.py
@@ -8,5 +8,36 @@
 # N(Mariners) Drive connection for Carisbatch running.
 ###########################################################################
 
+import os
+import os.path
+import oracledb
+from shapely import geometry
+from osgeo import ogr, osr, gdal
+import yaml
+
+Save = 'C:\\Temp\\chart\\single\\'
+
+def main():  
+    
+    with open('config.yml', 'r') as file:
+        HPDConnection = yaml.safe_load(file)
+        user = HPDConnection['HPDConnection']['User']
+        password = HPDConnection['HPDConnection']['PW']
+        dsn = HPDConnection['HPDConnection']['dsn'] +':'+ HPDConnection['HPDConnection']['port'] +'/'+ HPDConnection['HPDConnection']['DBname']
+        Dbname = HPDConnection['HPDConnection']['DBname']    
+            
+        Charts = HPDConnection['Datasets']['Charts']
+        for chart in Charts:
+            chartname = chart
+            print(chartname)
+                    
+    file.close()
+
+
+if __name__ == "__main__":
+    main()
+    
+
+
 
 
